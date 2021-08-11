3c40e018a723199707caa2f3a2c4941a620d119c
Nate Bruggeman <njbruggeman@gmail.com>
2021-08-11 10:58:00 -0500

    book is now removable

--- app.js
+++ app.js
@@ -43,7 +43,12 @@ UI.prototype.showAlert = function(message, className){
 }
 
 
-
+//Delete book
+UI.prototype.deleteBook = function(target) {
+    if(target.className === 'delete'){
+        target.parentElement.parentElement.remove();
+    }
+}
 
 UI.prototype.clearFields = function(){
     document.getElementById('title').value = '';
@@ -76,6 +81,19 @@ document.getElementById('book-form').addEventListener('submit', function(e){
     //clear fields when added
     ui.clearFields();
 
+
+    e.preventDefault();
+});
+
+
+// event listener for delete
+document.getElementById('book-list').addEventListener('click', function(e){
+
+    const ui = new UI();
+
+    ui.deleteBook(e.target);
+
+    ui.showAlert('Book Removed', 'success');
 
     e.preventDefault();
 })
